a47e83322cf76322b59d8a692a4e62d81d2105b9
mani-shashi <shashi2043@gmail.com>
2025-12-29 15:39:17 +0530

    Added comments, region, refactored code

--- QuickMartTraders/Program.cs
+++ QuickMartTraders/Program.cs
@@ -4,18 +4,19 @@ using QuickmartTraders.Transaction;
 
 public class Program
 {
-    public static SaleTransaction? LastTransaction;
-    public static bool HasLastTransaction = false;
+    public static SaleTransaction? LastTransaction; //store last transaction
+    public static bool HasLastTransaction = false; // check if last transaction exist
     public static void Main(String[] args)
     {
         while(true)
         {
-        
+            #region Menu
             Console.WriteLine("\n\n=============== QuickMart Traders ===============");
             Console.WriteLine("1. Create New Transaction (Enter Purchase & Selling Details)");
             Console.WriteLine("2. View Last Transaction");
             Console.WriteLine("3. Calculate Profit/Loss (Recompute & Print)");
             Console.WriteLine("4. Exit\n\n");
+            #endregion
             Console.WriteLine("Enter your option: ");
 
             string? input = Console.ReadLine();
@@ -30,19 +31,17 @@ public class Program
             } 
             else if (choice == 3)
             {
-
-                if (!HasLastTransaction)
+                if (!HasLastTransaction) // check if transaction exist  
                 {
                     Console.WriteLine("No transaction available. Please create a new transaction first.");
                     continue;
                 }
-                LastTransaction.Calculate();
+                LastTransaction.Calculate(); // calculate last transaction
 
             }
             else if (choice == 2)
             {
-
-                if (!HasLastTransaction)
+                if (!HasLastTransaction) // check if transaction exist  
                 {
                     Console.WriteLine("No transaction available. Please create a new transaction first.");
                     continue;
@@ -52,16 +51,14 @@ public class Program
             } 
             else if (choice == 1)
             {
+                LastTransaction  = new SaleTransaction(); //create new saleTransaction obj
+                LastTransaction.Register(); //register transaction
 
-                LastTransaction  = new SaleTransaction();
-                LastTransaction.Register();
+                HasLastTransaction = true; //mark transaction exist
 
-                HasLastTransaction = true;
-
-                LastTransaction.Calculate();
+                LastTransaction.Calculate(); // calculate last transaction
                 Console.WriteLine("\nTransaction Saved Successfully\n");
-                LastTransaction.Summary();
-                
+                LastTransaction.Summary(); // display transaction summary
             }
             else
             {
--- QuickMartTraders/SaleTransaction.cs
+++ QuickMartTraders/SaleTransaction.cs
@@ -2,6 +2,7 @@ namespace QuickmartTraders.Transaction
 {
     public class SaleTransaction
     {
+        #region Fields
         public string? InvoiceNo{get; set;}
         public string? CustomerName{get; set;}
         public string? ItemName{get; set;}
@@ -15,9 +16,11 @@ namespace QuickmartTraders.Transaction
         public decimal ProfitOrLossAmount{get; set;}
         public decimal ProfitOrLossMarginPercent{get; set;}
         public string? ProfitOrLossStatus{get; set;}
+        #endregion
 
-
+        #region Method - Register()
         ///<summary>
+        /// Method to register the transaction
         /// </summary>
         public void Register()
         {
@@ -32,12 +35,12 @@ namespace QuickmartTraders.Transaction
 
             Console.WriteLine("Enter Quantity: ");
             string? input = Console.ReadLine();
-            int.TryParse(input, out int tempQuantity);
+            int.TryParse(input, out int tempQuantity); // parse user input as int
             Quantity = tempQuantity;
 
             Console.WriteLine("Enter Purchase Amount (total): ");
             input = Console.ReadLine();
-            Decimal.TryParse(input, out decimal tempPurchaseAmount);
+            Decimal.TryParse(input, out decimal tempPurchaseAmount); // safely parse user input as decimal
             PurchaseAmount = tempPurchaseAmount;
 
             Console.WriteLine("Enter Selling Amount (total): ");
@@ -45,6 +48,12 @@ namespace QuickmartTraders.Transaction
             Decimal.TryParse(input, out decimal tempSellingAmount);
             SellingAmount = tempSellingAmount;
         }
+        #endregion
+
+        #region Method - View()
+        /// <summary>
+        /// Method to view the last tranction
+        /// </summary>
         public void View()
         {  
                 Console.WriteLine("---------------Last Transaction--------------");
@@ -59,33 +68,41 @@ namespace QuickmartTraders.Transaction
                 Console.WriteLine($"Profit/Loss Margin(%): {ProfitOrLossMarginPercent}");
                 
                 Console.WriteLine("--------------------------------------");
-            
         }
+        #endregion
 
+        #region Method - Calculate()
+        /// <summary>
+        /// Method to calculate the transaction
+        /// </summary>
         public void Calculate()
         {
-            if (SellingAmount > PurchaseAmount)
+            if (SellingAmount > PurchaseAmount) // If in profit , means selling amount is greater than purchaseAmount
             {
-                ProfitOrLossStatus = "Profit";
-                ProfitOrLossAmount = Math.Round(SellingAmount - PurchaseAmount , 2);
-                //If selling amount is greater, return sellinAmount - PurchaseAmount rounded up to 2 decimals
+                ProfitOrLossStatus = "PROFIT";
+                ProfitOrLossAmount = Math.Round(SellingAmount - PurchaseAmount , 2); // Round ProfitAmount up to 2 decimals
+                
             } 
-            else if (SellingAmount < PurchaseAmount)
+            else if (SellingAmount < PurchaseAmount) // If in loss , means selling amount is less than purchaseAmount
             {
-                ProfitOrLossStatus = "Loss";
-                ProfitOrLossAmount = Math.Round(PurchaseAmount - SellingAmount, 2);
-                //If purchase amount is greater, return purchaseAmount - sellingAmount rounded up to 2 decimals
+                ProfitOrLossStatus = "LOSS";
+                ProfitOrLossAmount = Math.Round(PurchaseAmount - SellingAmount, 2); // Round LossAmount up to 2 decimals
             }
-            else
+            else // if neither profit nor loss
             {
-                ProfitOrLossStatus = "Break - Even";
+                ProfitOrLossStatus = "BREAK - EVEN";
                 ProfitOrLossAmount = 0;
             }
 
-            ProfitOrLossMarginPercent = Math.Round ((ProfitOrLossAmount / PurchaseAmount) * 100 , 2);
+            ProfitOrLossMarginPercent = Math.Round ((ProfitOrLossAmount / PurchaseAmount) * 100 , 2); 
+            //profit or loss margin percent (rounded upto 2 decimals)
         }
+        #endregion
 
-
+        #region Method - Summary()
+        /// <summary>
+        /// Method to print summary
+        /// </summary>
         public void Summary()
         {
             Console.WriteLine($"\nStatus: {ProfitOrLossStatus}");
@@ -94,5 +111,6 @@ namespace QuickmartTraders.Transaction
 
             Console.WriteLine("--------------------------------------\n");
         }
+        #endregion
     }
 }
